2f2ed0e872d12620e29f0003d330a6a1f52ce3b8
at <adriantabirta@gmail.com>
2023-12-06 20:36:39 +0200

    changes

---
+++ GenerateScripts/generate-grpc-files-for-swift.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+
+# Remove old generated files that can case errors on new generation
+
+rm -r ../../GeneratedFiles/*
+
+echo -e "${YELLOW}Removing old files...${NOCOLOR}"
+
+protoc --swift_out=Visibility=public,FileNaming=DropPath:../../GeneratedFiles \
+--grpc-swift_opt=Visibility=Public,FileNaming=DropPath,TestClient=true,Client=true,Server=false \
+--grpc-swift_out=../../GeneratedFiles \
+ProtoFiles/*.proto
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Successfuly generated swift files!${NOCOLOR}"
+    echo -e "${YELLOW}  - all files can be found in directory: '../../GeneratedFiles/${NOCOLOR}'"
+else
+    echo -e "${RED}Cannot generate files!!!${NOCOLOR}"
+fi
+
+# echo "Generating new files..."
+
+# echo "Done!"
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # open-taxi-service-proto
-Open taxi service proto files for GRPC communication
+
+OpenTaxiService proto all files for GRPC communications.
+This repository can be used by any tehnology to generate whole data layer including validation on server side.
+
+## GenerateScripts
+Contains diferent scripts for diferent languages to generate all necessary files including validation for server side go.
+
+To generate use one of the following scripts:
+
+```
+- ./generate-grpc-files-for-go.sh
+- ./generate-grpc-files-for-swift.sh
+```
+
+## ProtoFiles
+Contains all files describing whole service & model validation.
